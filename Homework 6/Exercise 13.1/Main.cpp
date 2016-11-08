d81d0453e55cb120d8e44491e060d180330258c6
Benjamin Morledge-Hampton <bmorledge-hampton19@my.whitworth.edu>
2016-11-08 15:13:14 -0800

    -Recreated git repository because it was having issues.  I think I'm getting the hang of this...

---
+++ Homework 6/Exercise 13.1/Main.cpp
@@ -0,0 +1,54 @@
+/////////////////////
+// Ben Morledge-Hampton
+// CS 172-1
+// 11-8-2016
+// Homework 6
+/////////////////////
+
+//Pre-Processor Directives
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+// Main function
+int main() {
+
+	// Seed the RNG with the current time.
+	srand((unsigned)time(NULL));
+
+	// Create an fstream for writing to the file.
+	fstream inout;
+
+	// Open a file in order to append to its current contents.
+	inout.open("Exercise13_1.txt", ios::out, ios::app);
+
+	// Make sure the file was opened successfully.
+	if (inout.fail()) {
+
+		// If there was an issue, make it known, and exit the program.
+		cout << "Something went horribly wrong." << endl;
+		return 1;
+
+	}
+
+	// Generate 100 random integers from 0-100, with a space between each number and a newline every 10 numbers.
+	for (int i = 0; i < 100; i++) {
+
+		// Append the generated integer to the file, followed by a space.
+		inout << rand() % 100 << " ";
+
+		// If this is the 10th, 20th, etc. iteration of the loop, add a newline!
+		if (i % 10 == 9) inout << endl;
+
+	}
+
+
+	// If everything worked out, make note of it!
+	cout << "Everything seems to have gone alright!";
+
+	// End function.  Everything is ok!
+	return 0;
+
+}
