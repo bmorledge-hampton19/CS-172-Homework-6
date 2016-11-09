a9aa3dd128f9d9d66fcf4c24afe879d916388a67
Benjamin Morledge-Hampton <bmorledge-hampton19@my.whitworth.edu>
2016-11-08 21:46:40 -0800

    -Created definition for Complex class.

---
+++ Homework 6/Exercise 14.7/Complex.h
@@ -0,0 +1,59 @@
+#pragma once
+
+#include <fstream>
+using namespace std;
+
+// Overloaded arithmetic operators that are non-member functions that act like the arithmetic member functions.
+Complex operator+(const Complex& c1, const Complex& c2);
+Complex operator-(const Complex& c1, const Complex& c2);
+Complex operator*(const Complex& c1, const Complex& c2);
+Complex operator/(const Complex& c1, const Complex& c2);
+
+
+class Complex {
+
+private:
+	double a; // The "a" in a + bi.  The real part of the complex number.
+	double b; // The "b" in a + bi.  The imaginary part of the complex number.
+
+public:
+	Complex(); // Creates a complex object for the number 0.
+	Complex(double a); // Creates a complex object with a given "a" and 0 as b.
+	Complex(double a, double b); // Creates a complex object with given values for "a" and "b".
+
+	// Functions to perform basic arithmetic operations with complex numbers as described by the book.
+	Complex add(Complex c) const;
+	Complex subtract(Complex c) const;
+	Complex multiply(Complex c) const;
+	Complex divide(Complex c) const;
+
+	Complex abs(); // Return the absolute value of a complex number.
+
+	//Overloading ALL the operators!!!!!
+	
+	// Operator overloading that uses the member arithmetic functions to perform the specified calcuation and return the value to the complex number that intiated the function.
+	void operator+=(Complex c);
+	void operator-=(Complex c);
+	void operator*=(Complex c);
+	void operator/=(Complex c);
+
+	// Operator overloading that returns a for [0] and b for [1]
+	int& operator[](int index);
+
+	// Unary operator overloading for complex numbers.
+	Complex operator+(); // Changes nothing before returning.
+	Complex operator-(); // Returns the complex object with -a and -b.
+
+	// Prefix operator overloading.  Only affects the a value.
+	Complex& operator++();
+	Complex& operator--();
+
+	// Postfix operator overloading.  Only affects the a value.
+	Complex operator++(int dummy);
+	Complex operator--(int dummy);
+
+	// Overloaded operators for the input and output operators.  Displays the complex number as a string, or reads in two values (a and b) for the Complex number.
+	friend ostream& operator<<(ostream& out, const Complex& c);
+	friend istream& operator>>(istream& in, Complex& c);
+
+};
---
+++ Homework 6/Exercise 14.7/Main.cpp
@@ -0,0 +1,8 @@
+/////////////////////
+// Ben Morledge-Hampton
+// CS 172-1
+// 11-8-2016
+// Homework 6
+/////////////////////
+
+//Pre-Processor Directives
